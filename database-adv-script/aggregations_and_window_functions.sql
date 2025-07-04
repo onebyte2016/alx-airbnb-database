afd762f488fee41f015964b5b4d9d91c7100d5cd
onebyte2016 <onebyte2016@gmail.com>
2025-07-04 11:42:27 +0000

    Rwnumber updated

--- database-adv-script/aggregations_and_window_functions.sql
+++ database-adv-script/aggregations_and_window_functions.sql
@@ -13,28 +13,29 @@ GROUP BY
 ORDER BY 
     total_bookings DESC;
 
+
 --2. Rank properties based on the total number of bookings using a window function
 
 
+-- SELECT 
+--     property_id,
+--     COUNT(*) AS total_bookings,
+--     RANK() OVER (ORDER BY COUNT(*) DESC) AS booking_rank
+-- FROM 
+--     bookings
+-- GROUP BY 
+--     property_id;
+
 SELECT 
     property_id,
-    COUNT(*) AS total_bookings,
-    RANK() OVER (ORDER BY COUNT(*) DESC) AS booking_rank
-FROM 
-    bookings
-GROUP BY 
-    property_id;
-
--- SELECT 
---     p.id AS property_id,
---     p.name,
---     COUNT(b.id) AS total_bookings,
---     RANK() OVER (ORDER BY COUNT(b.id) DESC) AS booking_rank
--- FROM 
---     properties p
--- LEFT JOIN 
---     bookings b ON p.id = b.property_id
--- GROUP BY 
---     p.id, p.name
--- ORDER BY 
---     booking_rank;
+    total_bookings,
+    ROW_NUMBER() OVER (ORDER BY total_bookings DESC) AS booking_rank
+FROM (
+    SELECT 
+        b.property_id,
+        COUNT(*) AS total_bookings
+    FROM 
+        bookings b
+    GROUP BY 
+        b.property_id
+) AS property_bookings;
